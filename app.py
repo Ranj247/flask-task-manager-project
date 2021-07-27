99c90af95748b4b85962b6138d436aa4f03fba3a
Ranj247 <ranjeet.sadioura@gmail.com>
2021-07-27 12:56:43 +0000

    Create base template

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
+    # generate data from our tasks collection on MongoDB
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
